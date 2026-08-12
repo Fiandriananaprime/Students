5696b88d5c269ea2b665833f69b72c7aee920b02
Fiandriananaprime <hei.fiandrianana@gmail.com>
2026-08-12 09:23:23 +0300

    prepare backend for deployment

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import StudentsRoute from "./routes/StudentsRoute";
 import { errorHandler } from "./middleware/errorHandler";
+import cors from 'cors'
 
 const app = express();
-const PORT = process.env.PORT ?? 8080;
+const PORT = Number(process.env.PORT) ?? 8080;
 
+app.use(cors({
+  origin:process.env.FRONTEND_URL
+}))
 app.use(express.json());
 
 app.use("/Students", StudentsRoute);
@@ -12,6 +16,6 @@ app.use("/Students", StudentsRoute);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",() => {
   console.log(`API démarrée sur http://localhost:${PORT}`);
 });
