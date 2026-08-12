0ab8a6524d39c083c86587ce7474df65f6f3106c
Fiandriananaprime <hei.fiandrianana@gmail.com>
2026-08-12 10:55:45 +0300

    fix: add CORS policy

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 const PORT = Number(process.env.PORT) ?? 8080;
 
 
-app.use(cors)
+app.use(cors({origin:process.env.FRONTEND_URL}))
 
 app.use(express.json());
 
@@ -16,6 +16,8 @@ app.use("/Students", StudentsRoute);
 
 app.use(errorHandler);
 
+console.log("PORT =", process.env.PORT);
+console.log("FRONTEND_URL =", process.env.FRONTEND_URL);
 app.listen(PORT, "0.0.0.0",() => {
   console.log(`API démarrée sur http://localhost:${PORT}`);
 });
